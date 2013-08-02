9ac7f11a3be6ad4f13b042c6df432d9133b51739
Xuhao <xuhao@rubyfans.com>
2013-08-02 18:32:12 +0800

    MISC - Add permission check for wiki save action

--- app/controllers/wulin_wiki/wikis_controller.rb
+++ app/controllers/wulin_wiki/wikis_controller.rb
@@ -4,6 +4,7 @@ module WulinWiki
     before_filter :get_wiki, only: :show
 
     def save
+      redirect_to(root_path) if respond_to?(:current_user) && current_user.admin?
       @wiki = Wiki.where(grid_name: params[:wiki][:grid_name]).first_or_initialize
       @wiki.assign_attributes(params[:wiki])
       @wiki.save
