e78cdacabc331d098bbd1a80c6c905f76588107c
Jimmy Huang <pake007@gmail.com>
2013-08-30 12:29:44 +0800

    Improve screen wiki dialog title

--- app/assets/javascripts/screen_wiki.js
+++ app/assets/javascripts/screen_wiki.js
@@ -6,13 +6,15 @@ WulinMaster.actions.ScreenWiki = $.extend({}, WulinMaster.actions.BaseAction, {
     // Remove all existing dialogs
     $("#screen_wiki_dialog").remove();
 
-    var $dialog, $wiki_container, screenName;
+    var $dialog, $wiki_container, screenName, screenTitle;
 
     screenName = this.screen_name;
+    screenTitle = this.screen_title;
+
     $wiki_container = $('<div/>').attr({'class': 'screen_wiki'});
 
     $dialog = $('<div/>')
-      .attr({'id': 'screen_wiki_dialog', 'title': 'Wiki for ' + screenName + ' screen'})
+      .attr({'id': 'screen_wiki_dialog', 'title': 'Wiki for ' + screenTitle + ' screen'})
       .css({display: 'none', 'overflow-y': 'auto'})
       .append($wiki_container)
       .appendTo('body');
