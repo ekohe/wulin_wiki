a244659880e472f3540cb7d878aa39e9791fce30
Xuhao <xuhao@rubyfans.com>
2013-09-04 19:41:41 +0800

    MISC - grid_name and screen_name validations corrections

--- app/models/wulin_wiki/wiki.rb
+++ app/models/wulin_wiki/wiki.rb
@@ -3,6 +3,7 @@ module WulinWiki
     attr_accessible :content, :grid_name, :user_email, :screen_name
 
     validates :content, :user_email, presence: true
-    validates :grid_name, :screen_name, uniqueness: true, allow_blank: true
+    validates_uniqueness_of :grid_name, allow_blank: true, allow_nil: true
+    validates_uniqueness_of :screen_name, allow_blank: true, allow_nil: true
   end
 end
