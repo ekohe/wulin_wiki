9554df79480067bb9e74af7848a2ac28b94d9ad8
Ben Zheng <ben@ekohe.com>
2016-12-30 11:25:43 -0800

    MISC #61502 Upgrade TerraNova
    - Stop using deprecated attr_accessible

--- app/models/wulin_wiki/wiki.rb
+++ app/models/wulin_wiki/wiki.rb
@@ -1,6 +1,6 @@
 module WulinWiki
   class Wiki < ActiveRecord::Base
-    attr_accessible :content, :grid_name, :user_email, :screen_name
+    # attr_accessible :content, :grid_name, :user_email, :screen_name
 
     validates_uniqueness_of :grid_name, allow_blank: true, allow_nil: true
     validates_uniqueness_of :screen_name, allow_blank: true, allow_nil: true
