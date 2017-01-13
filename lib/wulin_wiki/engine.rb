0afbfc20cc628e09a20cdfc0deceabad423d02ae
Ben Zheng <ben@ekohe.com>
2017-01-12 16:22:39 -0800

    FEATURE - Add :append_migrations initializer

--- lib/wulin_wiki/engine.rb
+++ lib/wulin_wiki/engine.rb
@@ -5,5 +5,13 @@ module WulinWiki
     initializer "add assets to precompile" do |app|
        app.config.assets.precompile += %w( wulin_wiki.js kindeditor.js en.js wulin_wiki.css wiki.png)
     end
+
+    initializer :append_migrations do |app|
+      unless app.root.to_s.match root.to_s
+        config.paths["db/migrate"].expanded.each do |expanded_path|
+          app.config.paths["db/migrate"] << expanded_path
+        end
+      end
+    end
   end
 end
